e2ed4ab62432812059fb6a1d016730052f769658
Rajinder Singh <l00157049@student.lyit.ie>
2021-03-23 13:50:16 +0000

    Initial commit of week 3 demo file

---
+++ week3demo.R
@@ -0,0 +1,6 @@
+#file downloaded from baclkboard loaded into df
+# missing data replaced by NA
+managers_data <- read.csv("managers.csv", na= "")
+
+#view the structure of the datafrmae
+str(managers_data)
